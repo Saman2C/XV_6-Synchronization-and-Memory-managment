91e36dc557f77af5c0abcc7323e6e2e028a96e92
zahra <z.darabi81@gmail.com>
2025-01-01 10:21:11 +0330

    Add reentrantlock

---
+++ reentrantlock.c
@@ -0,0 +1,59 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "x86.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "spinlock.h"
+
+
+struct reentrantlock {
+    struct spinlock lock;
+    struct proc *owner;
+    int recursion;
+};
+
+void
+initreentrantlock(struct reentrantlock *rlock, char *name)
+{
+    struct spinlock lk;
+    initlock(&lk, name);
+    rlock->lock = lk;
+    rlock->owner = 0;
+    rlock->recursion = 0;
+}
+
+void
+acquirereentrantlock(struct reentrantlock *rlock)
+{
+    struct spinlock *lk = &(rlock->lock);
+    acquire(lk);
+    if (rlock->owner == myproc())
+        rlock->recursion ++;
+    else
+    {
+        while (rlock->owner != 0)
+            sleep(rlock, lk);
+        rlock->owner = myproc();
+        rlock->recursion = 1;
+    }
+    release(lk);
+}
+
+void
+releasereentrantlock(struct reentrantlock *rlock)
+{
+    struct spinlock *lk = &(rlock->lock);
+    acquire(lk);
+    if (rlock->owner != myproc())
+        return;
+    rlock->recursion --;
+    if (rlock->recursion == 0)
+    {
+        rlock->owner = 0;
+        wakeup(rlock);
+    }
+    release(lk);
+}
+
